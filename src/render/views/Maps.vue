916e45d0c02660821929f15264de23522927e68d
nigelkeel <thevoe@qq.com>
2021-01-27 16:14:15 +0800

    feat: 自定义地图的样式 @0.15

--- src/render/views/Maps.vue
+++ src/render/views/Maps.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="maps main">
     <h1>地图</h1>
-    <el-card id="container">
-      <!-- <div class="boxer" id="container"></div> -->
+    <el-card >
+      <div id="container" style="width: 88vw; height: 76vh"></div>
     </el-card>
   </div>
 </template>
@@ -14,7 +14,17 @@ export default {
   },
   methods: {
     init() {
-      var map = new AMap.Map("container");
+      var map = new AMap.Map("container", {
+        zoom: 10, //默认的缩放级别
+        // center: [116.397428, 39.90923], //中心点坐标
+        // viewMode:'3D', //使用3D视图
+      });
+      map.setMapStyle('amap://styles/7bbfcf1f317256e306f370324e1167e0'); //设置样式
+      // 添加一个标记
+      var marker = new AMap.Marker({
+          position:[114.39, 23.9], //位置
+      })
+      map.add(marker);//添加到地图
       window.map = map;
     }
   }
